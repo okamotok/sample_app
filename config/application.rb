29733b4541c07cdcd1201d56034ad038dff80130
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-02-05 17:43:21 -0800

    complete tutorial up to 5.1.3

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,7 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    ### Add following line for bootstrap http://ruby.railstutorial.org/chapters/filling-in-the-layout#code-bootstrap_config
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
