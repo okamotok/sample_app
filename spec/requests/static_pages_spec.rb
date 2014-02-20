ad8bca75f72ad105f7fb6b141ed15d14bc3d4c2e
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-02-19 18:28:22 -0800

    add initial lab3 tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,88 @@
 require 'spec_helper'
 
 
-describe "xxStatic pages" do
-  describe "xxHome page" do
-    it "xxshould have the content 'Sample App'" do
+describe "Static pages" do
+
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "aaa should have the title 'Home'" do
+    it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("CS 232 Rails Development | Home")
     end
   end
 
-  describe "yyHelp page" do
-    it "zzshould have the content 'Help'" do
+  describe "Help page" do
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("CS 232 Rails Development | Help")
     end
   end
-  describe "About page" do
 
+  describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("CS 232 Rails Development | About Us")
     end
   end
-end
+
+  describe "Contact page" do
+    it "should have title 'CS 232 Rails Development | Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("CS 232 Rails Development | About Us")
+    end
+
+    it "should contain the words 'CS 232 Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content("CS 232 Contact")
+    end
+
+    it "should contain an H1 element with class 'page-title'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('<H1 class="page-title"')
+    end
+
+    it "should contain a definition list with a <dl>" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('<dl>')
+    end
+
+    it "should contain a <dt> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('<dt>')
+    end
+
+    it "should contain a <dd> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('<dd>')
+    end
+
+    it "should contain a <section class='main'> element" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('<section class="main">')
+    end
+
+    it "should contain a 'h1.page-title' selector in the CSS" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('h1.page-title')
+    end
+
+    it "should contain a '.main' selector in the CSS" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('.main')
+    end
+
+  end
+
 end
 
