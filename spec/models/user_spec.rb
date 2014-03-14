424b03be44cad303962924aef123a15bbf5bd9d0
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-03-14 14:40:18 -0700

    add password and confirmation  6.3.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  before {@user = User.new(name: "Example User", email: "user@example.com")}
+  before { @user = User.new(name: "Example User", email: "user@example.com",
+                            password: "foobar", password_confirmation: "foobar")}
 
   subject { @user }
 
   it {should respond_to(:name) }
   it {should respond_to(:email) }
+  it {should respond_to(:password_digest)}
+  it {should respond_to(:password)}
+  it {should respond_to(:password_confirmation)}
   it {should be_valid}
 
   describe "when name is not present" do
@@ -54,4 +58,18 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                    password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password does not match confirmation" do
+    before { @user.password_confirmation = "mismatch" }
+    it { should_not be_valid }
+  end
+
 end
