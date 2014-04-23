e7860bb6e55bb071427540a5dce5015ec09b0a34
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-04-23 15:18:53 -0700

    8.2.1  add remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_create :create_remember_token
 
   #callback to convert all :email attributes to lower case before saving
   before_save { self.email = email.downcase }
@@ -9,4 +10,18 @@ class User < ActiveRecord::Base
                     uniqueness: {case_sensitive: false}
   has_secure_password
   validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.digest(User.new_remember_token)
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token) }
   it {should respond_to(:authenticate)}
   it {should be_valid}
 
@@ -77,6 +78,7 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
+
   describe "return value of authentication method" do
     before { @user.save }
     let (:found_user) { User.find_by(email: @user.email) }
@@ -89,9 +91,11 @@ describe User do
       let (:user_for_invalid_password) { found_user.authenticate("invalid")}
       specify { expect(user_for_invalid_password).to be_false }
     end
-
   end
 
-
+  describe "remember_token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank}
+  end
 
 end
