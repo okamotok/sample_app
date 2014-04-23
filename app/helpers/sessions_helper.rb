f01bd26929178847e06d35b837cec6e29e995873
Kenji Okamoto <kokamoto@mail.ccsf.edu>
2014-04-23 15:30:37 -0700

    8.2.3  add methods to get and set current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,15 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute :remember_token, User.digest(remember_token)
-    self.current_user = user
+    self.current_user = user   #self is the controller object
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,17 +11,18 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
-  def User.new_remember_token
+  def self.new_remember_token #can also do def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
-  def User.digest(token)
+  def self.digest(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
 
   private
 
     def create_remember_token
+      #self is an instance of User class
       self.remember_token = User.digest(User.new_remember_token)
     end
 end
